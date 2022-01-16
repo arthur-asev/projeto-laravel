64f68990b4cc9b1ef2bd4d9fb61f2e989f9b53a1
Arthur Rodrigues <arthurginho02@gmail.com>
2022-01-16 20:34:59 -0300

    primeiro commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpdatePost;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::get();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request)
+    {
+        Post::create($request->all());
+
+        return redirect()->route('posts.index');
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+        Post::where('id', $id)->first();
+        if (!$post) {
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        if (!$post = Post::find($id))
+            return redirect()->route('posts.index');
+
+        $post->delete();
+
+        return redirect()
+            ->route(('posts.index'))
+            ->with('message', 'Post Deletado com sucesso');
+    }
+
+    public function home()
+    {
+        echo "teste";
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,24 @@
+<h1>Cadastrar novo Post</h1>
+
+@if ($errors->any())
+<div>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+
+
+
+</div>
+@endif
+
+<form method="POST" action='{{ route('posts.store')}}'>
+
+    <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
+    @csrf
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{old('content')}}</textarea>
+    <button type="submit">Enviar</button>
+
+
+</form>
